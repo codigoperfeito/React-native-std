13f11874b6e1df9c21631924d6ea00578dcd03f5
Wesley Almeida <obilhao@gmail.com>
2022-03-20 21:22:34 -0300

    Study: Style in project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
 
 
 import CompPadrao, {Comp1,Comp2} from './components/Multi'
 import Primeiro from './components/primeiro'
 
+
 export default () => (
-    <SafeAreaView>
+    <SafeAreaView style={style.App}>
         <CompPadrao/>
         <Comp1/>
         <Comp2/>
         <Primeiro/>
     </SafeAreaView>
 )
+const style = StyleSheet.create({
+    App:{
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+}) 
--- src/components/Multi.js
+++ src/components/Multi.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Estilo from './estilo'
 
 function comp () {
-    return <Text>Comp #Oficial </Text>
+    return <Text style={Estilo.txtG}>Comp #Oficial </Text>
 }
 function Comp1 () {
-    return <Text>Comp #01 </Text>
+    return <Text style={Estilo.txtG}>Comp #01 </Text>
 }   
 function Comp2 () {
-    return <Text>Comp #02 </Text>
+    return <Text style={Estilo.txtG}>Comp #02 </Text>
 }
 
 export { Comp1,Comp2}
